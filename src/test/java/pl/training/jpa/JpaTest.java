70286cd97b77faeef0f3a7f8ea90037c8ba72607
landrzejewski <landrzejewski.poczta@gmail.com>
2022-04-04 11:32:51 +0200

    base

--- src/test/java/pl/training/jpa/JpaTest.java
+++ src/test/java/pl/training/jpa/JpaTest.java
@@ -1,15 +1,27 @@
 package pl.training.jpa;
 
+import org.hibernate.LazyInitializationException;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static pl.training.jpa.TestUtils.ENTITY_MANAGER_FACTORY;
-import static pl.training.jpa.TestUtils.run;
+import static org.junit.jupiter.api.Assertions.*;
+import static pl.training.jpa.TestUtils.*;
 
 class JpaTest {
 
     private final Client client = Fixtures.testClient();
+    private Client clientProxy;
+
+    @BeforeEach
+    void beforeEach() {
+        STATISTICS.clear();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        ENTITY_MANAGER_FACTORY.close();
+    }
 
     @Test
     void given_an_entity_object_when_persist_then_entity_state_is_saved_into_database() {
@@ -23,30 +35,90 @@ class JpaTest {
 
     @Test
     void given_an_attached_entity_when_entity_state_is_changed_then_entity_state_is_automatically_synchronized_with_the_database() {
+        var newFirstName = "Marek";
+        run(entityManager -> {
+            entityManager.persist(client);
+            // entityManager.flush();
+            client.setFirstName(newFirstName);
+        });
+        run(entityManager -> {
+            var persistedClient = entityManager.find(Client.class, client.getId());
+            assertEquals(newFirstName, persistedClient.getFirstName());
+        });
     }
 
     @Test
     void given_a_detached_entity_when_merge_then_entity_state_is_automatically_synchronized_with_the_database_and_managed_entity_is_returned() {
+        var newFirstName = "Marek";
+        run(entityManager -> entityManager.persist(client));
+        run(entityManager -> {
+            client.setFirstName(newFirstName);
+            var managedClient = entityManager.merge(client);
+        });
+        run(entityManager -> {
+            var persistedClient = entityManager.find(Client.class, client.getId());
+            assertEquals(newFirstName, persistedClient.getFirstName());
+        });
     }
 
     @Test
     void given_an_attached_entity_when_remove_then_entity_state_is_removed_from_database() {
+        run(entityManager -> entityManager.persist(client));
+        run(entityManager -> {
+            var persistedClient = entityManager.find(Client.class, client.getId());
+            entityManager.remove(persistedClient);
+        });
+        run(entityManager -> assertNull(entityManager.find(Client.class, client.getId())));
     }
 
     @Test
     void given_a_detached_entity_when_remove_then_throws_an_exception() {
+        run(entityManager -> {
+            entityManager.persist(client);
+            entityManager.detach(client);
+            // entityManager.clear();
+            assertThrows(IllegalArgumentException.class, () -> entityManager.remove(client));
+        });
     }
 
     @Test
     void given_an_entity_when_refresh_then_entity_state_is_synchronized_with_database_state() {
+        var newFirstName = "Marek";
+        run(entityManager -> entityManager.persist(client));
+        run(entityManager -> {
+            var persistedClient = entityManager.find(Client.class, client.getId());
+            updateClientAsync(newFirstName);
+            sleep(500);
+            persistedClient.setFirstName("Test name");
+            entityManager.refresh(persistedClient);
+            assertEquals(newFirstName, persistedClient.getFirstName());
+        });
+    }
+
+    private void updateClientAsync(String newFirstName) {
+        runAsync(entityManager -> {
+            var persistedClient = entityManager.find(Client.class, client.getId());
+            persistedClient.setFirstName(newFirstName);
+        });
     }
 
     @Test
     void given_a_persisted_entity_when_get_reference_then_entity_state_is_lazy_loaded() {
+        run(entityManager -> entityManager.persist(client));
+        run(entityManager -> {
+            clientProxy = entityManager.getReference(Client.class, client.getId());
+            var clientId = clientProxy.getId();
+            assertEquals(0, STATISTICS.getEntityLoadCount());
+            var clientFirstName = clientProxy.getFirstName();
+            assertEquals(1, STATISTICS.getEntityLoadCount());
+        });
     }
 
     @Test
     void given_a_persisted_entity_when_get_reference_after_transaction_ic_closed_then_throws_an_exception() {
+        run(entityManager -> entityManager.persist(client));
+        run(entityManager -> clientProxy = entityManager.getReference(Client.class, client.getId()));
+        assertThrows(LazyInitializationException.class, () -> clientProxy.getFirstName());
     }
 
     @Test
@@ -99,11 +171,6 @@ class JpaTest {
 
     @Test
     void given_two_transactions_when_first_transactions_acquired_the_lock_then_second_transaction_waits_for_first_transaction_to_release_the_lock() throws InterruptedException {
-    }
-
-    @AfterAll
-    static void afterAll() {
-        ENTITY_MANAGER_FACTORY.close();
     }
 
 }
--- src/test/java/pl/training/jpa/TestUtils.java
+++ src/test/java/pl/training/jpa/TestUtils.java
@@ -3,12 +3,15 @@ package pl.training.jpa;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.SessionFactory;
+import org.hibernate.stat.Statistics;
 
 import java.util.function.Consumer;
 
 class TestUtils {
 
     static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("training");
+    static final Statistics STATISTICS = ENTITY_MANAGER_FACTORY.unwrap(SessionFactory.class).getStatistics();
 
     static void run(Consumer<EntityManager> task) {
         var entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
@@ -17,6 +20,18 @@ class TestUtils {
         task.accept(entityManager);
         transaction.commit();
         entityManager.close();
+    }
+
+    static void runAsync(Consumer<EntityManager> task) {
+        new Thread(() -> run(task)).start();
+    }
+
+    static void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
     }
 
 }
